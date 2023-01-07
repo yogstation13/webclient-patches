b71d2c5cb730e748a5beba56e969bd3dafb9f921
monster860 <mnmaxim@gmail.com>
2023-01-07 01:00:33 -0500

    God damn it I pulled a stupid

--- src/regparm3_bullshit.c
+++ src/regparm3_bullshit.c
@@ -4,12 +4,12 @@
 #define REGPARM3 __attribute__((regparm(3)))
 #endif
 
-void REGPARM3 receive_login_key_hook(void* a, short b);
+void receive_login_key_hook(void* a, short b);
 void REGPARM3 _receive_login_key_hook_c(void* a, short b) {
 	receive_login_key_hook(a, b);
 }
 
 void (REGPARM3 *_receive_login_key_orig)(void* a, short b);
-void REGPARM3 _receive_login_key_orig_c(void* a, short b) {
+void _receive_login_key_orig_c(void* a, short b) {
 	_receive_login_key_orig(a, b);
 }
